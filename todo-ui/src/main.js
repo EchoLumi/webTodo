f979fd9e3956de502ec5916a5c7423872b327069
EchoLumi <18076111486@163.com>
2025-08-05 23:39:24 +0800

    修改todo页面结构

--- todo-ui/src/main.js
+++ todo-ui/src/main.js
@@ -1,9 +1,11 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
 
+// 引入element-plus和样式
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 createApp(App).use(router).mount('#app')
--- todo-ui/src/views/Todo.vue
+++ todo-ui/src/views/Todo.vue
@@ -5,9 +5,17 @@ export default {
 </script>
 
 <template>
-
+<div class="main">
+  <div class="todo-card">
+    <div class="card-left"></div>
+    <div class="card-right"></div>
+  </div>
+</div>
 </template>
 
 <style scoped>
-
+*{
+  margin: 0;
+  padding: 0;
+}
 </style>
